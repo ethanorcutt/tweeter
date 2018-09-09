9a107434900f98194ad36d978e6d4368bbbee570
Ethan Orcutt <izzaboy306@gmail.com>
2018-09-09 17:47:06 -0500

    Used some partials and helpers to reduce the logic in the controller. Also got the additional parameters working for devise.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,17 @@
 module PagesHelper
+  def pick_user_profile_button
+    if user_signed_in?
+      render './layouts/partials/logged_in_user'
+    else
+      render './layouts/partials/logged_out_user'
+    end
+  end
+
+  def greeting_helper
+    if user_signed_in?
+      "Hello, #{current_user.full_name}"
+    else
+      "Hello, Guest"
+    end
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,11 @@
 module PostsHelper
   def has_user_liked(post)
-    if !current_user.liked? post
-      link_to '', like_post_path(post), class: 'like-btn far fa-heart', method: :put, remote: :true
-    elsif current_user.liked? post
-      link_to '', like_post_path(post), class: 'like-btn fas fa-heart', method: :put, remote: :true
+    if user_signed_in?
+      if !current_user.liked? post
+        link_to '', like_post_path(post), class: 'like-btn far fa-heart', method: :put, remote: :true
+      elsif current_user.liked? post
+        link_to '', like_post_path(post), class: 'like-btn fas fa-heart', method: :put, remote: :true
+      end
     end
   end
 
